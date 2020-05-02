ab8c0cc3fef641fa34a262f88af82533db09ec96
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-05-02 18:29:03 +0100

    Added basic comments

--- main.c
+++ main.c
@@ -21,10 +21,11 @@ int main() {
     return 0;
 }
 
+/*Runs all the tests*/
 void runTests() {
     CU_initialize_registry();
-    CU_pSuite average_suite = CU_add_suite("average_suite", 0, 0);
-    CU_pSuite max_suite = CU_add_suite("max_suite", 0, 0);
+    CU_pSuite average_suite = CU_add_suite("average_suite", 0, 0);//Suite for average function
+    CU_pSuite max_suite = CU_add_suite("max_suite", 0, 0);//Suite for max function
 
     /*Tests for the average function*/
     CU_ADD_TEST(average_suite, average_test_normal);
@@ -43,6 +44,7 @@ void runTests() {
     CU_cleanup_registry();
 }
 
+/*Tests basic inputs for average*/
 void average_test_normal() {
     double test1[] = {1.1, 2.2, 3.3, 4.4};
     CU_ASSERT_DOUBLE_EQUAL(average(test1, 4), 2.75, 0.0001);
@@ -52,6 +54,7 @@ void average_test_normal() {
     CU_ASSERT_DOUBLE_EQUAL(average(test3, 6), 3.5, 0.0001);
 }
 
+/*Tests when all values are the same*/
 void average_test_same() {
     double test1[10];
     for (int i = 0; i < 10; i++) {
@@ -65,11 +68,13 @@ void average_test_same() {
     CU_ASSERT_DOUBLE_EQUAL(average(test2, 15), 3.1415, 0.0001);
 }
 
+/*Tests when there are no values in the array*/
 void average_test_empty() {
     double test1[] = {};
     CU_ASSERT(isnan(average(test1, 0)));
 }
 
+/*Tests when there is only a single value in the array*/
 void average_test_single() {
     double test1[] = {4};
     CU_ASSERT_DOUBLE_EQUAL(average(test1, 1), 4, 0.0001);
@@ -77,6 +82,7 @@ void average_test_single() {
     CU_ASSERT_DOUBLE_EQUAL(average(test2, 1), -0.404, 0001)
 }
 
+/*Tests basic inputs for the max*/
 void max_test_normal() {
     double test1[] = {1.1, 2.2, 3.3, 4.4};
     CU_ASSERT_DOUBLE_EQUAL(max(test1, 4), 4.4, 0.0001);
@@ -86,6 +92,7 @@ void max_test_normal() {
     CU_ASSERT_DOUBLE_EQUAL(max(test3, 6), 6, 0.0001);
 }
 
+/*Tests when all values are the same*/
 void max_test_same() {
     double test1[10];
     for (int i = 0; i < 10; i++) {
@@ -104,11 +111,13 @@ void max_test_same() {
     CU_ASSERT_DOUBLE_EQUAL(max(test3, 50), 1.414213, 0.0001);
 }
 
+/*Tests when there are no values in the array*/
 void max_test_empty() {
     double test1[] = {};
     CU_ASSERT_DOUBLE_EQUAL(max(test1, 0), -INFINITY, 0.0001);
 }
 
+/*Tests when there's only one value in the array*/
 void max_test_single() {
     double test1[] = {4};
     CU_ASSERT_DOUBLE_EQUAL(max(test1, 1), 4, 0.0001);
